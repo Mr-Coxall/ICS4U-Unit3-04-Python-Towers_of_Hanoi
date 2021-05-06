6a70b02b91a130f0cf4662f124b1e78d15964cc1
Patrick Coxall <mr.coxall@mths.ca>
2021-05-06 18:54:25 +0000

    init

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ICS4U-Unit3-04-Python-Towers_of_Hanoi
+[![GitHub's Super Linter](https://github.com/Mr-Coxall/ICS4U-Unit3-04-Python-Towers_of_Hanoi/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/Mr-Coxall/ICS4U-Unit3-04-Python-Towers_of_Hanoi/actions)
+
+[![Run on Repl.it](https://repl.it/badge/github/Mr-Coxall/ICS4U-Unit3-04-Python-Towers_of_Hanoi)](https://repl.it/github/Mr-Coxall/ICS4U-Unit3-04-Python-Towers_of_Hanoi)
---
+++ towers_of_hanoi.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+# Created by: Mr. Coxall
+# Created on: Sep 2020
+# This program is the Towers of Hanoi program
+
+
+def hanoi(ndisks, startPeg=1, endPeg=3):
+    # this is the recursive Towers of Hanoi solution
+
+    if ndisks == 1:
+        print("Move disk 1 from peg %d to peg %d" % (startPeg, endPeg))
+    else:
+        hanoi(ndisks-1, startPeg, 6-startPeg-endPeg)
+        print("Move disk %d from peg %d to peg %d" % (ndisks, startPeg, endPeg))
+        hanoi(ndisks-1, 6-startPeg-endPeg, endPeg)
+
+def main():
+    # this function calculates area and perimeter
+
+    # input
+    print("Towers of Hanoi program\n")
+    number_of_disks = int(input("How many disks would you like in your tower (1->): "))
+
+    # process
+    hanoi(ndisks=number_of_disks)
+
+    # output
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
